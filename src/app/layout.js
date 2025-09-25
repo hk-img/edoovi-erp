2f8f19d7e8544241aa59ee2dd415786d3790fd85
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2025-09-25 11:12:47 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,30 @@
 import "./globals.css";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import Header from "@/components/comman/Header";
 import Footer from "@/components/comman/Footer";
 
-const lufga = localFont({
-  src: [
-    { path: "/fonts/Lufga-Thin.woff2", weight: "100", style: "normal" },
-    { path: "/fonts/Lufga-ThinItalic.woff2", weight: "100", style: "italic" },
-    { path: "/fonts/Lufga-ExtraLight.woff2", weight: "200", style: "normal" },
-    { path: "/fonts/Lufga-ExtraLightItalic.woff2", weight: "200", style: "italic" },
-    { path: "/fonts/Lufga-Light.woff2", weight: "300", style: "normal" },
-    { path: "/fonts/Lufga-LightItalic.woff2", weight: "300", style: "italic" },
-    { path: "/fonts/Lufga-Regular.woff2", weight: "400", style: "normal" },
-    { path: "/fonts/Lufga-Italic.woff2", weight: "400", style: "italic" },
-    { path: "/fonts/Lufga-Medium.woff2", weight: "500", style: "normal" },
-    { path: "/fonts/Lufga-MediumItalic.woff2", weight: "500", style: "italic" },
-    { path: "/fonts/Lufga-SemiBold.woff2", weight: "600", style: "normal" },
-    { path: "/fonts/Lufga-SemiBoldItalic.woff2", weight: "600", style: "italic" },
-    { path: "/fonts/Lufga-Bold.woff2", weight: "700", style: "normal" },
-    { path: "/fonts/Lufga-BoldItalic.woff2", weight: "700", style: "italic" },
-    { path: "/fonts/Lufga-ExtraBold.woff2", weight: "800", style: "normal" },
-    { path: "/fonts/Lufga-ExtraBoldItalic.woff2", weight: "800", style: "italic" },
-    { path: "/fonts/Lufga-Black.woff2", weight: "900", style: "normal" },
-    { path: "/fonts/Lufga-BlackItalic.woff2", weight: "900", style: "italic" },
-  ],
-});
+// const lufga = localFont({
+//   src: [
+//     { path: "/fonts/Lufga-Thin.woff2", weight: "100", style: "normal" },
+//     { path: "/fonts/Lufga-ThinItalic.woff2", weight: "100", style: "italic" },
+//     { path: "/fonts/Lufga-ExtraLight.woff2", weight: "200", style: "normal" },
+//     { path: "/fonts/Lufga-ExtraLightItalic.woff2", weight: "200", style: "italic" },
+//     { path: "/fonts/Lufga-Light.woff2", weight: "300", style: "normal" },
+//     { path: "/fonts/Lufga-LightItalic.woff2", weight: "300", style: "italic" },
+//     { path: "/fonts/Lufga-Regular.woff2", weight: "400", style: "normal" },
+//     { path: "/fonts/Lufga-Italic.woff2", weight: "400", style: "italic" },
+//     { path: "/fonts/Lufga-Medium.woff2", weight: "500", style: "normal" },
+//     { path: "/fonts/Lufga-MediumItalic.woff2", weight: "500", style: "italic" },
+//     { path: "/fonts/Lufga-SemiBold.woff2", weight: "600", style: "normal" },
+//     { path: "/fonts/Lufga-SemiBoldItalic.woff2", weight: "600", style: "italic" },
+//     { path: "/fonts/Lufga-Bold.woff2", weight: "700", style: "normal" },
+//     { path: "/fonts/Lufga-BoldItalic.woff2", weight: "700", style: "italic" },
+//     { path: "/fonts/Lufga-ExtraBold.woff2", weight: "800", style: "normal" },
+//     { path: "/fonts/Lufga-ExtraBoldItalic.woff2", weight: "800", style: "italic" },
+//     { path: "/fonts/Lufga-Black.woff2", weight: "900", style: "normal" },
+//     { path: "/fonts/Lufga-BlackItalic.woff2", weight: "900", style: "italic" },
+//   ],
+// });
 
 export const metadata = {
   title: "Create Next App",
@@ -34,7 +34,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="md:text-[1vw]">
-      <body className={`${lufga.className} antialiased`}>
+      <body>
         <Header />
         {children}
         <Footer />
